ce60ec318ca0c4ab25c56bc7994e38a299dcccdf
MauricioNovak <mauricionovak03@gmail.com>
2024-11-10 11:10:06 -0300

    add all project

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+const precio = 400000
+let cantidad = 0;
+let total = 0;
+
+let botonMas = document.querySelector(".btnMas")
+let botonMenos = document.querySelector(".btnMenos")
+
+let cantidadSpan = document.querySelector('.cantidad');
+let totalSpan = document.querySelector('.valor-total');
+let precioSpan = document.querySelector(".precio-inicial")
+
+totalSpan.innerHTML = total
+precioSpan.innerHTML = precio
+
+botonMas.addEventListener('click', function() {
+    cantidad++;  
+    cantidadSpan.innerHTML = cantidad; 
+    total = precio * cantidad
+    totalSpan.innerHTML = total
+});
+
+botonMenos.addEventListener('click', function() {
+    if (cantidad > 0) {  
+        cantidad--; 
+        cantidadSpan.innerHTML = cantidad;  
+        total = precio * cantidad
+        totalSpan.innerHTML = total
+    }
+});
+
